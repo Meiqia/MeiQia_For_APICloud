b9767ba3540939f103ec6cc5fee0ae56ce36571e
InjoyDeng <Injoy@DengInjoydeMacBook-Pro.local>
2016-01-28 19:02:30 +0800

    config testApp

---
+++ iOS/TestApp/UZApp/AppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.m
+//  UZApp
+//
+//  Created by kenny on 14-2-18.
+//  Copyright (c) 2014年 APICloud. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@end
